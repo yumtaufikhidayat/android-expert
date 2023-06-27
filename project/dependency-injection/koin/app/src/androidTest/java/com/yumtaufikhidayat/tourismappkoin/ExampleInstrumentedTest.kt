3c1a60064a58e8eb99fa8827e4d9b615722ab891
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-06-27 18:44:37 +0700

    Implement project: Import Tourism App Flow.

--- project/dependency-injection/koin/app/src/androidTest/java/com/yumtaufikhidayat/tourismappkoin/ExampleInstrumentedTest.kt
+++ project/dependency-injection/koin/app/src/androidTest/java/com/yumtaufikhidayat/tourismappkoin/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.yumtaufikhidayat.tourismappkoin", appContext.packageName)
+        assertEquals("com.yumtaufikhidayat.tourismappflow", appContext.packageName)
     }
 }
