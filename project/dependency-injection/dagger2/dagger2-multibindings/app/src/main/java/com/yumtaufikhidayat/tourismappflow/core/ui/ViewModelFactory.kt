8d9c447d81d4e7e9664b2e7d426a2b6051ed880c
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-06-30 15:02:18 +0700

    Implement project: Adjust class to enable multibinding viewModel.

--- project/dependency-injection/dagger2/dagger2-multibindings/app/src/main/java/com/yumtaufikhidayat/tourismappflow/core/ui/ViewModelFactory.kt
+++ project/dependency-injection/dagger2/dagger2-multibindings/app/src/main/java/com/yumtaufikhidayat/tourismappflow/core/ui/ViewModelFactory.kt
@@ -2,27 +2,18 @@ package com.yumtaufikhidayat.tourismappflow.core.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.yumtaufikhidayat.tourismappflow.core.domain.usecase.TourismUseCase
-import com.yumtaufikhidayat.tourismappflow.detail.DetailTourismViewModel
-import com.yumtaufikhidayat.tourismappflow.favorite.FavoriteViewModel
-import com.yumtaufikhidayat.tourismappflow.home.HomeViewModel
 import javax.inject.Inject
+import javax.inject.Provider
 
-class ViewModelFactory @Inject constructor(private val tourismUseCase: TourismUseCase) :
-    ViewModelProvider.NewInstanceFactory() {
+class ViewModelFactory @Inject constructor(
+    private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) :
+    ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        when {
-            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
-                HomeViewModel(tourismUseCase) as T
-            }
-            modelClass.isAssignableFrom(FavoriteViewModel::class.java) -> {
-                FavoriteViewModel(tourismUseCase) as T
-            }
-            modelClass.isAssignableFrom(DetailTourismViewModel::class.java) -> {
-                DetailTourismViewModel(tourismUseCase) as T
-            }
-            else -> throw Throwable("Unknown ViewModel class: " + modelClass.name)
-        }
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val creator = creators[modelClass] ?: creators.entries.firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value ?: throw IllegalArgumentException("Unknown model class $modelClass")
+        return creator.get() as T
+    }
 }
