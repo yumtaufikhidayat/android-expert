90dff65664f7ed94f85422f0a517798ada3197e2
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-06-30 15:01:02 +0700

    Implement project: Enable constructor inject to each class.

--- project/dependency-injection/dagger2/dagger2-multibindings/app/src/main/java/com/yumtaufikhidayat/tourismappflow/detail/DetailTourismViewModel.kt
+++ project/dependency-injection/dagger2/dagger2-multibindings/app/src/main/java/com/yumtaufikhidayat/tourismappflow/detail/DetailTourismViewModel.kt
@@ -3,8 +3,9 @@ package com.yumtaufikhidayat.tourismappflow.detail
 import androidx.lifecycle.ViewModel
 import com.yumtaufikhidayat.tourismappflow.core.domain.model.Tourism
 import com.yumtaufikhidayat.tourismappflow.core.domain.usecase.TourismUseCase
+import javax.inject.Inject
 
-class DetailTourismViewModel(
+class DetailTourismViewModel @Inject constructor(
     private val tourismUseCase: TourismUseCase
 ) : ViewModel() {
     fun setFavoriteTourism(tourism: Tourism, newStatus: Boolean) {
--- project/dependency-injection/dagger2/dagger2-multibindings/app/src/main/java/com/yumtaufikhidayat/tourismappflow/favorite/FavoriteViewModel.kt
+++ project/dependency-injection/dagger2/dagger2-multibindings/app/src/main/java/com/yumtaufikhidayat/tourismappflow/favorite/FavoriteViewModel.kt
@@ -3,8 +3,9 @@ package com.yumtaufikhidayat.tourismappflow.favorite
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.yumtaufikhidayat.tourismappflow.core.domain.usecase.TourismUseCase
+import javax.inject.Inject
 
-class FavoriteViewModel(
+class FavoriteViewModel @Inject constructor(
     tourismUseCase: TourismUseCase
 ) : ViewModel() {
     val favoriteTourism = tourismUseCase.getFavoriteTourism().asLiveData()
