d71e891d43b3857b4b6a4202aac6276b1c04abf7
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-02 10:52:56 +0700

    Implement practice: Fix qualifier name on Simple Login app.

--- practice/modularization/modularization/app/src/main/java/com/yumtaufikhidayat/simplelogin/HomeActivity.kt
+++ practice/modularization/modularization/app/src/main/java/com/yumtaufikhidayat/simplelogin/HomeActivity.kt
@@ -3,19 +3,20 @@ package com.yumtaufikhidayat.simplelogin
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.yumtaufikhidayat.core.UserRepository
 import com.yumtaufikhidayat.simplelogin.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
-    private var userRepository: com.yumtaufikhidayat.core.UserRepository? = null
+    private var userRepository: UserRepository? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         val session = com.yumtaufikhidayat.core.SessionManager(this)
-        userRepository = com.yumtaufikhidayat.core.UserRepository.getInstance(session)
+        userRepository = UserRepository.getInstance(session)
 
         binding.tvWelcome.text = String.format("%s %s", "Welcome", "${userRepository?.getUser()}")
 
--- practice/modularization/modularization/app/src/main/java/com/yumtaufikhidayat/simplelogin/MainActivity.kt
+++ practice/modularization/modularization/app/src/main/java/com/yumtaufikhidayat/simplelogin/MainActivity.kt
@@ -3,19 +3,20 @@ package com.yumtaufikhidayat.simplelogin
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.yumtaufikhidayat.core.UserRepository
 import com.yumtaufikhidayat.simplelogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private var userRepository: com.yumtaufikhidayat.core.UserRepository? = null
+    private var userRepository: UserRepository? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         val session = com.yumtaufikhidayat.core.SessionManager(this)
-        userRepository = com.yumtaufikhidayat.core.UserRepository.getInstance(session)
+        userRepository = UserRepository.getInstance(session)
 
         if (userRepository?.isUserLogin() == true) {
             moveToHomeActivity()
