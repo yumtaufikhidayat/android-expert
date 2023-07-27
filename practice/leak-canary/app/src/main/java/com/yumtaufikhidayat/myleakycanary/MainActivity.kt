83f2d5be757509c09d8d91c93901dbd2f6483634
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-27 12:34:04 +0700

    Implement practice: Implement leak canary for memory leak monitoring.

--- practice/leak-canary/app/src/main/java/com/yumtaufikhidayat/myleakycanary/MainActivity.kt
+++ practice/leak-canary/app/src/main/java/com/yumtaufikhidayat/myleakycanary/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.yumtaufikhidayat.myleakycanary
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yumtaufikhidayat.myleakycanary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var broadcastReceiver: BroadcastReceiver? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+
+    private fun registerBroadcastReceiver() {
+        broadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                binding.apply {
+                    when (intent.action) {
+                        Intent.ACTION_POWER_CONNECTED -> tvPowerStatus.text = getString(R.string.power_connected)
+                        Intent.ACTION_POWER_DISCONNECTED -> tvPowerStatus.text = getString(R.string.power_disconnected)
+                    }
+                }
+            }
+        }
+        val intentFilter = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+        registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadcastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
     }
 }
