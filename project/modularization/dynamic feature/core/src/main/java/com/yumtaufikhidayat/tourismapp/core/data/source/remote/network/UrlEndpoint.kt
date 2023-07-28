f8d5fac248ba98f920f9198d47480c16c0bdb442
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-29 05:52:43 +0700

    Implement project: Init project.

--- project/modularization/dynamic feature/core/src/main/java/com/yumtaufikhidayat/tourismapp/core/data/source/remote/network/UrlEndpoint.kt
+++
@@ -1,6 +0,0 @@
-package com.yumtaufikhidayat.tourismapp.core.data.source.remote.network
-
-object UrlEndpoint {
-    const val BASE_URL = "https://tourism-api.dicoding.dev/"
-    const val GET_LIST = "list"
-}
--- project/modularization/dynamic feature/maps/src/main/java/com/yumtaufikhidayat/maps/MapsViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.yumtaufikhidayat.maps
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import com.yumtaufikhidayat.tourismapp.core.domain.usecase.TourismUseCase
-
-class MapsViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
-    val tourism = tourismUseCase.getAllTourism().asLiveData()
-}
