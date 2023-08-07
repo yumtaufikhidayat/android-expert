845013873f8533061733c228ff8b21d78d8f7707
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-08 06:00:48 +0700

    Implement practice: Add mapbox.

--- practice/mapbox/app/src/main/java/com/yumtaufikhidayat/mymapbox/MainActivity.kt
+++ practice/mapbox/app/src/main/java/com/yumtaufikhidayat/mymapbox/MainActivity.kt
@@ -2,14 +2,63 @@ package com.yumtaufikhidayat.mymapbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
 import com.yumtaufikhidayat.mymapbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var mapboxMap: MapboxMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        showMap()
+    }
+
+    private fun showMap() {
+        binding.mapView.getMapAsync {
+            this.mapboxMap = it
+            it.setStyle(Style.MAPBOX_STREETS) { streets ->
+                //
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
     }
 }
